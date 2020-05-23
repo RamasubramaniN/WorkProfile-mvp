00fbb1ff23648f4d03da238b276ffb167cb96d8d
Ramasubramani N <RAMASUBRAMANI_N@intuit.com>
2020-05-23 14:21:07 +0530

    Initial commit - Rest Backend

---
+++ linkedin-rest-backend/src/main/java/com/psg/ramasubramanin/linkedn/LinkedinApplication.java
@@ -0,0 +1,13 @@
+package com.psg.ramasubramanin.linkedn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinApplication.class, args);
+	}
+
+}
---
+++ linkedin-rest-backend/src/main/java/com/psg/ramasubramanin/linkedn/controller/LinkedinProfileController.java
@@ -0,0 +1,7 @@
+package com.psg.ramasubramanin.linkedn.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LinkedinProfileController {
+}
